01186d1bc72953ed7abeaab67c4566e377a8b7b4
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-08-01 16:48:59 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hpe",
-    version="1.1.0",
+    version="1.4.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
