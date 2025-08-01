ece9ed863aeed1154ee72377a0f1c98b06cdb197
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-08-01 16:32:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
             "hpe=hpe.cli:main",
         ],
     },
-    author="XO Aria, S.U.P.E, Alireza Enhessari",
+    author="S.U.P.E, Alireza Enhessari",
     author_email="hf18950@gmail.com, hamedsheygh3130011@gmail.com",
     description="Hamid Py Engine",
     license="MIT",
