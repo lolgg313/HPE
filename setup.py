3fa8522439569298cfcd44aa93f592a78dc61da7
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-08-01 15:53:22 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="hpe",
     version="1.0.0",
-    description="Hamid Py Engine",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="XO Aria, S.U.P.E",
-    author_email="hf18950@gmail.com, hamedsheygh@gmail.com",
-    license="MIT",
-    url="https://github.com/xo-aria/HPE",
-    project_urls={
-        "Homepage": "https://github.com/xo-aria/HPE",
-        "Repository": "https://github.com/xo-aria/HPE",
-        "Issues": "https://github.com/xo-aria/HPE/issues",
-    },
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -31,6 +19,25 @@ setup(
         "toml==0.10.2",
         "trimesh==4.7.1"
     ],
+    entry_points={
+        "console_scripts": [
+            "hpe=pyengine.cli:main",
+        ],
+    },
+    author="XO Aria, S.U.P.E",
+    author_email="hf18950@gmail.com, hamedsheygh@gmail.com",
+    description="A CLI and GUI-based Python Engine for Games and Graphics",
+    license="MIT",
+    url="https://github.com/xo-aria/HPE",
+    project_urls={
+        "Homepage": "https://github.com/xo-aria/HPE",
+        "Repository": "https://github.com/xo-aria/HPE",
+        "Issues": "https://github.com/xo-aria/HPE/issues",
+    },
+    package_data={
+        "pyengine": ["code/pyengine.py"],
+    },
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -40,9 +47,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Framework :: Pygame",
         "Topic :: Games/Entertainment :: Simulation",
-        "Framework :: Pygame"
     ],
-    python_requires=">=3.8",
 )
