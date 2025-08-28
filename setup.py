21afe30b4b7e26c77d074aa3a0d20604f660948c
S.U.P.E <hamedsheygh3130011@gmail.com>
2025-08-28 22:51:31 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hpe",
-    version="2.2.95",
+    version="2.3.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
